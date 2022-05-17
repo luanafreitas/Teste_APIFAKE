be19edc7038b1904495d0204089f5580cda7e9cb
luanafreitas <luanaluciele0@gmail.com>
2022-05-17 16:50:22 -0300

    primeiro commit

---
+++ cypress/integration/tests/firsttest.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+it('loads list of users', () => {
+    cy.intercept('/users').as('users')
+    cy.visit('/')
+    cy.wait('@users').its('response.body')
+      .should('be.an', 'Array')
+      .and('have.length.gt', 5)
+      .then(users => {
+        cy.get('[data-testid=user]')
+          .should('have.length', users.length)
+      })
+  })
---
+++ readme.md
@@ -0,0 +1,3 @@
+Install Cypress: npm install cypress
+Open Cypress Project: npx cypress open
+Open Cypress mobile: npx cypress open --config viewportWidth=375,viewportHeight=667
